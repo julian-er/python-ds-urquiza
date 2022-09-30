4431ab98296f8508fb8571c8bd3767c8fa49f448
Julián Rosalen <rosalenjulian@gmail.com>
2022-09-30 16:32:39 -0300

    new top level window class

---
+++ tkinter_new_window.py
@@ -0,0 +1,59 @@
+from tkinter import *
+
+
+class new_window(Toplevel):
+    '''
+        Define Tkinter Toplevel windows and configurations
+
+        Parameter:
+        title (str) : title of window -> has default if value not provided
+        iconUrl (str) : route of window icon -> has default if value not provided
+        cols (int) : number of columns on your window
+        rows (int) : number of rows on your window
+
+        Return tk toplevel , to execute window do new_window(tk.root)
+    '''
+     
+    def __init__(self, master = None, type_window= 'new_window', title='DS Urquiza',  iconUrl='./icons/j1.ico', cols=12, rows=12, ):
+        
+
+        # define variables
+        super().__init__(master = master)
+        self.title(title + ' second term practice')
+        self.iconbitmap(iconUrl)
+        self.col = cols
+        self.row = rows
+        self.type = type_window
+        self.minsize(500, 500)
+        self.resizable(False, False)
+        label = Label(self, text =self.type)
+        label.pack()
+
+    def define_cols_quantity(self):
+        '''
+            Choose quantity columns for your grid
+
+            Parameter:
+            col (int) : default 12
+        '''
+        for x in range(self.col):
+            self.columnconfigure(x, weight=1, uniform="x")
+
+    def define_rows_quantity(self):
+        '''
+            Choose quantity columns for your grid
+
+            Parameter:
+            col (int) : default 12
+        '''
+        for x in range(self.row):
+            self.rowconfigure(x, weight=1, uniform="x")
+
+
+
+
+        # label = Label(self, text ="Your user and password aren't valid")
+        # label.pack()
+
+        # label = Label(self, text ="This is a new Window")
+        # label.pack()
