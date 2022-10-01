848563368d52f3b980f6480a44cee083b58d2b9c
Julián Rosalen <rosalenjulian@gmail.com>
2022-10-01 15:56:41 -0300

    Fourth exercise WIP

--- SecondProject/fourth_exercise.py
+++ SecondProject/fourth_exercise.py
@@ -1,2 +1,16 @@
 # 4- Utilizando los widgets Menu y Notebook diseñar una pantalla de Menú/ Solapas de la
 #    temática que desees.
+
+
+import sys
+sys.path.append(".")
+from tkinter import *
+from tkinter.ttk import *
+from tkinter_basics import tkinter_basics
+
+## declare exercise 1 and props
+ej_4 = tkinter_basics(title='Fourth exercise - ')
+ej_4.can_resize(False)
+ej_4.root.config(background='#594A7D')
+
+ej_4.execute_window()
